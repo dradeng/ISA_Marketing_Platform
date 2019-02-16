8d8c7ab044392a2300445383b71af93c3bd5aee6
dradeng <dsg2dm@virginia.edu>
2019-02-16 12:25:51 -0500

    new models

--- app/isa/isa/models.py
+++ app/isa/isa/models.py
@@ -3,5 +3,11 @@ from django.db import models
 class ad(models.Model):
     image = models.FileField(upload_to='documents/%Y/%m/%d')
     duration = models.IntegerField(default=0)
-    Seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
-    
+    seller = models.ForeignKey(seller, on_delete=models.CASCADE)
+
+
+class seller(models.Model):
+    username = models.TextField(max_length=500)
+    name = models.TextField(max_length=500)
+    email = models.EmailField(max_length=254)
+    password = models.IntegerField(default=0)
