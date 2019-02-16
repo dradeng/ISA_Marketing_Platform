489d38418873dcc3513c5eeaa38084212c1e92e1
dradeng <dsg2dm@virginia.edu>
2019-02-16 12:42:38 -0500

    models updated

--- app/isa/isa/models.py
+++ app/isa/isa/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
-class ad(models.Model):
+class Ad(models.Model):
     image = models.FileField(upload_to='documents/%Y/%m/%d')
     duration = models.IntegerField(default=0)
-    seller = models.ForeignKey(seller, on_delete=models.CASCADE)
+    Seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    cost = models.DecimalField(max_digits=20, decimal_places=2)
 
 
-class seller(models.Model):
+class Seller(models.Model):
     username = models.TextField(max_length=500)
     name = models.TextField(max_length=500)
     email = models.EmailField(max_length=254)
     password = models.IntegerField(default=0)
+    company = models.TextField(max_length=500)
