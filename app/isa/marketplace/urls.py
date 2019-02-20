684d301026b2505faa40b1d566c4537ce59bc408
dradeng <dsg2dm@virginia.edu>
2019-02-19 19:43:15 -0500

    api routes

--- app/isa/marketplace/urls.py
+++ app/isa/marketplace/urls.py
@@ -8,6 +8,20 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', views.home, name='home'),
+    path('api/v1/ad/', views.adGet, name='ad_get'),
+    path('api/v1/ad/update/', views.adUpdate, name='ad_upate'),
+    path('api/v1/ad/delete/', views.adDelete, name='ad_delete'),
+    path('api/v1/ad/create/', views.adCreate, name='ad_create'),
+    path('api/v1/buyer/', views.buyerGet, name='buyer_get'),
+    path('api/v1/buyer/update/', views.buyerUpdate, name='buyer_update'),
+    path('api/v1/buyer/delete/', views.buyerDelete, name='buyer_delete'),
+    path('api/v1/buyer/create/', views.buyerCreate, name='buyer_create'),
+    path('api/v1/seller/', views.sellerGet, name='seller_get'),
+    path('api/v1/seller/update/', views.sellerUpdate, name='seller_update'),
+    path('api/v1/seller/delete/', views.sellerDelete, name='seller_delete'),
+    path('api/v1/seller/create/', views.sellerCreate, name='seller_create'),
+
+    #trevors code
     path('adCreate/', views.adCreate, name='ad_create'),
     path('<int:ad_id>/detail', views.ad_detail, name='ad_detail'),
 ]
--- app/isa/marketplace/views.py
+++ app/isa/marketplace/views.py
@@ -6,80 +6,6 @@ from django.contrib.auth import authenticate, login
 from .forms import AdForm
 
 
-
-
-# @route   POST views.login
-# @desc    Login user
-# @access  Public
-def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        #redirect
-        return HttpResponseRedirect('/')
-    #invalid login
-    return HttpResponseRedirect('/')
-
-# @route   Get views.home
-# @desc    Render home page
-# @access  Private
-def home(request):
-    ads = Ad.objects.all()
-    return render(request, 'home.html',
-        {'ads': ads })
-
-# @route   POST views.userCreate
-# @desc    Login user
-# @access  Private
-def userCreate(request):
-    if(request.POST['company'] is not None and request.POST['company'] != ''):
-        newUser = User.create(
-            username=request.POST['username'],
-            name=request.POST['name'],
-            email=request.POST['email'],
-            password=request.POST['password'],
-            company=request.POST['company']
-        )
-    else:
-        newUser = User.create(
-            username=request.POST['username'],
-            name=request.POST['name'],
-            email=request.POST['email'],
-            password=request.POST['password'],
-        )
-    newUser.save()
-    return
-
-# @route   DELETE views.userDelete
-# @desc    Delete a user
-# @access  Private
-def userDelete(request):
-    User.objects.filter(id=request.DELETE['id']).delete()
-    return
-
-
-# @route   POST views.userUpdate
-# @desc    update user info
-# @access  Private
-def userUpdate(request):
-    updatedUser = request.user
-    updatedUser.password = request.POST['password'] if request.POST['password'] else updatedUser.password
-    updatedUser.username = request.POST['username'] if request.POST['username'] else updatedUser.username
-    updatedUser.email = request.POST['email'] if request.POST['email'] else updatedUser.email
-    updatedUser.company = request.POST['company'] if request.POST['company'] else updatedUser.company
-    updatedUser.name = request.POST['name'] if request.POST['name'] else updatedUser.name
-    updatedUser.save()
-    return
-
-# @route   GET views.userGet
-# @desc    return user object
-# @access  Private
-def userGet(request):
-    return request.user
-
-
 # @route   POST views.adCreate
 # @desc    Create an ad
 # @access  Private
@@ -122,11 +48,137 @@ def adUpdate(request):
     updatedAd.save()
     return
 
+
 # @route   GET views.adGet
 # @desc    render individual ad
 # @access  Public
 def adGet(request):
     return Ad.objects.get(id=request.POST['id'])
+
+
+
+
+# @route   POST views.adCreate
+# @desc    Create an ad
+# @access  Private
+def buyerCreate(request):
+    if request.method == "POST":
+        image=request.POST['image'],
+        duration=request.POST['duration'],
+        user=request.user,
+        cost=request.POST['cost'],
+        url = request.POST['url'],
+        site_title = request.POST['site_title']
+        form_data={'image' : 'iamge', 'duration' : 'duration', 'cost' : 'cost', 'url' : 'url', 'site_title' : 'site_title'}
+        form = AdForm(data = form_data)
+        if(form.is_valid):
+            #User = user.objects.get(user_id=request.user.id)
+            createdAd = Ad(image=image, duration=duration, user=user, cost=cost, url=url, site_title=site_title)
+            createdAd.save()
+            return redirect('home')
+    return redirect('home')
+
+
+# @route   DELETE views.adDelete
+# @desc    delete ad
+# @access  Private
+def buyerDelete(request):
+    Ad.objects.filter(id=request.DELETE['id']).delete()
+    return
+
+
+# @route   POST views.adUpdate
+# @desc    Update an ad
+# @access  Private
+def buyerUpdate(request):
+    updatedAd = Ad.objects(id = request.POST['id'])
+    updatedAd.image = request.POST['image'] if request.POST['image'] else updatedAd.image
+    updatedAd.duration = request.POST['duration'] if request.POST['duration'] else updatedAd.duration
+    updatedAd.cost = request.POST['cost'] if request.POST['cost'] else updatedAd.cost
+    updatedAd.url = request.POST['url'] if request.POST['url'] else updatedAd.url
+    updatedAd.site_title = request.POST['site_title'] if request.POST['site_title'] else updatedAd.site_title
+    updatedAd.save()
+    return
+
+
+# @route   GET views.adGet
+# @desc    render individual ad
+# @access  Public
+def buyerGet(request):
+    return Ad.objects.get(id=request.POST['id'])
+
+
+
+
+
+# @route   POST views.adCreate
+# @desc    Create an ad
+# @access  Private
+def sellerCreate(request):
+    if request.method == "POST":
+        image=request.POST['image'],
+        duration=request.POST['duration'],
+        user=request.user,
+        cost=request.POST['cost'],
+        url = request.POST['url'],
+        site_title = request.POST['site_title']
+        form_data={'image' : 'iamge', 'duration' : 'duration', 'cost' : 'cost', 'url' : 'url', 'site_title' : 'site_title'}
+        form = AdForm(data = form_data)
+        if(form.is_valid):
+            #User = user.objects.get(user_id=request.user.id)
+            createdAd = Ad(image=image, duration=duration, user=user, cost=cost, url=url, site_title=site_title)
+            createdAd.save()
+            return redirect('home')
+    return redirect('home')
+
+
+# @route   DELETE views.adDelete
+# @desc    delete ad
+# @access  Private
+def sellerDelete(request):
+    Ad.objects.filter(id=request.DELETE['id']).delete()
+    return
+
+
+# @route   POST views.adUpdate
+# @desc    Update an ad
+# @access  Private
+def sellerUpdate(request):
+    updatedAd = Ad.objects(id = request.POST['id'])
+    updatedAd.image = request.POST['image'] if request.POST['image'] else updatedAd.image
+    updatedAd.duration = request.POST['duration'] if request.POST['duration'] else updatedAd.duration
+    updatedAd.cost = request.POST['cost'] if request.POST['cost'] else updatedAd.cost
+    updatedAd.url = request.POST['url'] if request.POST['url'] else updatedAd.url
+    updatedAd.site_title = request.POST['site_title'] if request.POST['site_title'] else updatedAd.site_title
+    updatedAd.save()
+    return
+
+
+# @route   GET views.adGet
+# @desc    render individual ad
+# @access  Public
+def sellerGet(request):
+    return Ad.objects.get(id=request.POST['id'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @route   Get views.home
+# @desc    Render home page
+# @access  Private
+def home(request):
+    ads = Ad.objects.all()
+    return render(request, 'home.html',
+        {'ads': ads })
 
 # @route   GET views.userCreate
 # @desc    return ad info
