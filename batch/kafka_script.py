b35c249fe31356725ab96d54a783a4bacb7666c2
dradeng <dsg2dm@virginia.edu>
2019-04-03 22:14:40 -0400

    PENIS FUCK TREVOR

--- batch/kafka_script.py
+++ batch/kafka_script.py
@@ -3,35 +3,28 @@ import json
 import time
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch(['es'])
-ad = {"user": 1, "image": "", "duration": 10, "cost": 20.00, "url": "www.google.com", "site_title": "Google", "id": 1}
-es.index(index='listing_index', doc_type='listing', id=ad['id'], body=ad)
-es.indices.refresh(index="listing_index")
-print("hi")
-
-c = None #KafkaConsumer
-es = None #ElasticSearch
-while c is None:
+consumer = None
+es = None
+while consumer is None:
     try:
-        c = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+        consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
         es = Elasticsearch(['es'])
     except:
         time.sleep(1)
 
-es_load = False
-ad = {"user": 1, "image": "", "duration": 10, "cost": 20.00, "url": "www.google.com", "site_title": "Google", "id": 1}
+loaded_es = False
+ad1 = {"user": 1, "image": "", "duration": 10, "cost": 20.00, "url": "www.google.com", "site_title": "Google", "id": 1}
 time.sleep(10)
-while not es_load:
+while not loaded_es:
     try:
-        es.index(index='listing_index', doc_type='listing', id=ad['id'], body=ad)
+        es.index(index='listing_index', doc_type='listing', id=ad1['id'], body=ad1)
         es.indices.refresh(index="listing_index")
-        es_load = True
+        loaded_es = True
     except:
         time.sleep(1)
 
-#consistent loop
 while True:
-    for message in c:
+    for message in consumer:
         message_dict = json.loads((message.value).decode('utf-8'))
         es.index(index='listing_index', doc_type='listing', id=message_dict['id'], body=message_dict)
     es.indices.refresh(index="listing_index")
