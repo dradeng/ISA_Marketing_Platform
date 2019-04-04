1ff17a508da231eda57f61f95f37e6aab6b14691
han-gu <hg5mc@virginia.edu>
2019-04-03 22:33:36 -0400

    lets goooooooo

--- batch/kafka_script.py
+++ batch/kafka_script.py
@@ -13,11 +13,13 @@ while consumer is None:
         time.sleep(1)
 
 loaded_es = False
-ad1 = {"user": 1, "image": "", "duration": 10, "cost": 20.00, "url": "www.google.com", "site_title": "Google", "id": 1}
+ad1 = {"user": 1, "image": "google.com", "duration": 10, "cost": 30.00, "url": "http://google.com", "site_title": "Google", "id": 1}
+ad2 = {"user": 2, "image": "facebook.com", "duration": 15, "cost": 20.00, "url": "http://facebook.com", "site_title": "Facebook", "id": 2}
 time.sleep(10)
 while not loaded_es:
     try:
         es.index(index='listing_index', doc_type='listing', id=ad1['id'], body=ad1)
+        es.index(index='listing_index', doc_type='listing', id=ad2['id'], body=ad2)
         es.indices.refresh(index="listing_index")
         loaded_es = True
     except:
