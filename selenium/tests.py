e49d5dfd9c9fb99130425a766ef9104cbc4dd45b
dradeng <dsg2dm@virginia.edu>
2019-04-15 19:02:06 -0400

    updated selenium tests

--- selenium/tests.py
+++ selenium/tests.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import NoSuchElementException
 
@@ -16,6 +17,42 @@ class TestTemplate(unittest.TestCase):
     def tearDown(self):
         """Stop web driver"""
         self.driver.quit()
+
+
+    def test_book_link(self):
+        """Find and click ad title button"""
+        try:
+            self.driver.get('http://frontend:8000/')
+            el = self.driver.find_element_by_class_name('adClass')
+            el.click()
+            info = self.driver.findElement(By.tagName("h1"));
+            contains_title = "Google" in info.get_attribute("innerHTML")
+            self.assertTrue(contains_title)
+        except NoSuchElementException as ex:
+            self.fail(ex.msg)
+
+    def test_login_logout(self):
+        """Log in using web interface"""
+        try:
+            self.driver.get('http://frontend:8000/login')
+            password = self.driver.find_element_by_id('id_password')
+            password.send_keys("password")
+            username = self.driver.find_element_by_id('id_email')
+            username.send_keys("dradengaff@gmail.com")
+
+            submit = self.driver.find_element_by_class_name("btn-lg")
+            submit.click()
+            self.assertEqual(self.driver.title, "Homepage")
+
+            logout = self.driver.find_element_by_partial_link_text("Logout")
+            self.assertEqual(logout.get_attribute("href"), "http://frontend:8000/logout")
+            logout.click()
+
+            login_button = self.driver.find_element_by_partial_link_text("Login")
+            self.assertEqual(login_button.get_attribute("href"), "http://frontend:8000/login")
+        except NoSuchElementException as ex:
+            self.fail(ex.msg)
+
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestTemplate)
